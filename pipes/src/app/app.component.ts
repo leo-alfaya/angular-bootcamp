b0d6c5b51b854d4dab1bc9b724f082bb76487538
Leonardo Alfaya <leo.alfaya@gmail.com>
2021-06-17 20:10:26 -0300

    finish pipes project

--- pipes/src/app/app.component.ts
+++ pipes/src/app/app.component.ts
@@ -6,5 +6,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+  name: string = '';
+  date: string = '';
+  amount: number = 0;
+  height: number = 0;
+  miles: number = 0;
+
+  car = {
+    make: 'Toyota',
+    model: 'Camry',
+    year: 2000
+  }
+  
+  onNameChange(event: any) {
+    this.name = event.target.value
+  }
+
+  onDateChange(event: any) {
+    this.date = event.target.value
+  }
+
+  onAmountChange(event: any) {
+    this.amount = parseFloat(event.target.value)
+  }
+
+  onHeightChange(event: any) {
+    this.height = parseFloat(event.target.value)
+  }
+
+  onMilesChange(event: any) {
+    this.miles = parseFloat(event.target.value)
+  }
 }
